36b1e6d522f69ac721088aa4f8f083cbee43ee56
mjrmonothumb <passjbw7553@gmail.com>
2025-06-16 11:31:53 -0400

    Finish initial game loop and add .gitignore

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+from pygame.locals import *
+
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	running = True
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+		screen.fill("black")
+		pygame.display.flip()
+
+if __name__== "__main__":
+	main()
+
+
